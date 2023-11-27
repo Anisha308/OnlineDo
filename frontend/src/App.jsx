cec2dc2da6ff4c8a19b856317c254fe09b10280d
Anisha <anishaanuu308@gmail.com>
2023-11-27 11:35:59 +0530

    created folder

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <>
+      <h1 className='text-[20px] text-blue-500'>helloooeeeeeeeeeeee</h1>
+    </>
+  );
+}
+
+export default App;
