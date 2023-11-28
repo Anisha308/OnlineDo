b07a3cc43b4db77a80806ed846364bbae81daf3a
Anisha <anishaanuu308@gmail.com>
2023-11-28 17:00:35 +0530

    backend of user added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import  './app.css'
 
 function App() {
   return (
     <>
-      <h1 className='text-[20px] text-blue-500'>helloooeeeeeeeeeeee</h1>
+      <h1 className='text-[20px] text-green-500'>helloooeeeeeeeeeeee</h1>
     </>
   );
 }
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,10 +1,22 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./index.html", "./src/**/*.{html,js}"],
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      // Add your custom styles or extend existing styles here
+      colors: {
+        primaryColor: '#0067FF',
+        yellowColor: '#FEB60D',
+        purpleColor: '#9771FF',
+        irisBlueColor: '#01B5C5',
+        headingColor: '#181A1E',
+        textColor:'#4E545F'
+      },
+      boxShadow: {
+        panelShadow:'rgba(17,12,46,0.15) 0px 48px 100px 0px;',
+        
+      }
     },
   },
   plugins: [],
 };
+
