0d71e702055805ae85d1e7aed946ca4a87a83bd1
Anisha <anishaanuu308@gmail.com>
2023-12-04 22:44:16 +0530

    user register,userlogin

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,8 @@
 import React from "react";
+import { Container } from "react-bootstrap";
+import { Outlet } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import  './app.css'
 import Layout from "./layout/layout";
 
@@ -6,8 +10,10 @@ import Layout from "./layout/layout";
 function App() {
   return (
     <>
+      
+
+      <ToastContainer />
       <Layout />
-     
     </>
   ); 
   
