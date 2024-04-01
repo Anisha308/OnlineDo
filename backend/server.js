a7f74aa3dedfe0b877e879883db3d74fe70b861c
Anisha <anishaanuu308@gmail.com>
2024-04-01 18:25:10 +0530

    updted

--- backend/server.js
+++ backend/server.js
@@ -47,6 +47,11 @@ app.use(bodyParser.urlencoded({ extended: true, limit: "10mb" }));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use((req, res, next) => {
+  res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
+  res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
+  next();
+});
 
 app.use("/api/users", userRoutes);
 app.use("/api/admin", adminRoutes);
